5f7c7d3b73b28ea5ff1e422c09b4a35b9104b5aa
bag-man <garland.owen@gmail.com>
2014-03-21 18:45:18 +0000

    Added interface

---
+++ code/GameModelInterface.java
@@ -0,0 +1,34 @@
+public interface GameModelInterface
+{
+  //returns what the user is allowed to see
+  public String getVisible();
+
+  //returns the words to guess
+  public String getHidden() ;
+
+  //tells how many guesses are left
+  public int guessLeft() ;
+
+  //returns the letters that have already been guessed
+  public String getLetters() ;
+
+  /**
+  *this method sees if the letter is in the words
+  *if it is it updates what the user can see (visible)
+  *if not it removes a guess
+  
+  *@param letter the letter to try
+  *@return whether there is winner
+  */
+  public boolean tryThis(char letter);
+
+  /**
+  * this method sees if the word guess is correct
+  * if it is it updates what the user can see (visible)
+  * if not it removes 5 guesses
+  *
+  * @param guess the word to guess
+  * @return whether there is winner
+  */
+  public boolean tryWord(String guess) ;
+}
