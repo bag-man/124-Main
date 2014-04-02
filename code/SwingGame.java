893b8d73c0ee41c897a7c2869a5338771e1fd826
bag-man <garland.owen@gmail.com>
2014-04-02 16:22:22 +0000

    Added a button!

--- code/SwingGame.java
+++ code/SwingGame.java
@@ -1,4 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 /* Remember to turn on window decor!!! */
 
@@ -13,6 +18,23 @@ public class SwingGame extends JFrame {
     setSize(500, 500);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);        
+
+    JPanel panel = new JPanel();
+    getContentPane().add(panel);
+
+    panel.setLayout(null);
+
+    JButton submitButton = new JButton("Submit");
+    submitButton.setBounds(0, 0, 120, 30); //X, Y, W, H
+
+    submitButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent event) {
+        System.out.println("Perform an action here!");
+      }
+    });
+
+    panel.add(submitButton);
 
   }
 
