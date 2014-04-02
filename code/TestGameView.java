fd0ac7514da1356f48df73105dcec14f1f1385ed
bag-man <garland.owen@gmail.com>
2014-04-02 19:07:22 +0000

    Re-jiggling test class

--- code/TestGameView.java
+++ code/TestGameView.java
@@ -2,29 +2,32 @@ import java.io.IOException;
 import javax.swing.SwingUtilities;
 
 public class TestGameView {
+   
+  private static Dictionary wordlist;
+  private static GameModel model;
 
   public static void main(String[] args) {
+
     try {
-
-
-      SwingUtilities.invokeLater(new Runnable() {
-
-	Dictionary wordlist = new Dictionary("../piratewords.txt");
-	GameModel model = new GameModel(wordlist);
-	//TextGame theGame = new TextGame(model); 
-
-	@Override
-	public void run() {
-	  SwingGame theGame = new SwingGame(model);
-	  theGame.setVisible(true);
-	}
-
-      });
-
-
+      wordlist = new Dictionary("../piratewords.txt");
     } catch (IOException e) {
       System.out.println("Error " + e);
     }
+
+    model = new GameModel(wordlist);
+
+    //TextGame theGame = new TextGame(model); 
+
+    SwingUtilities.invokeLater(new Runnable() {
+
+      @Override
+      public void run() {
+	SwingGame theGame = new SwingGame(model);
+	theGame.setVisible(true);
+      }
+
+    });
+
   }
 
 }
