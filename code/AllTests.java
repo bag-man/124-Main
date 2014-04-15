ad08f78d46aa5d83b744593430027adf6d0c4848
bag-man <garland.owen@gmail.com>
2014-04-15 14:43:20 +0000

    Junit testing done. I hope.

--- code/AllTests.java
+++ code/AllTests.java
@@ -22,10 +22,46 @@ public class AllTests {
   }
 
   @Test
-  public void hiddenWord() { 
+  public void testVisible() { 
+    assertEquals("Word has not been censored.", model.getVisible(), "****");
+    model.tryThis('a');
+    assertEquals("Word has not had a character revealed.", model.getVisible(), "a***");
+  }
 
+  @Test
+  public void testHidden() { 
     assertEquals("Word has not loaded correctly.", model.getHidden(), "ahoy");
+  }
 
+  @Test
+  public void testGuessLeft() { 
+    assertEquals("Guesses haven't been set at 10.", model.guessLeft(), 10);
+    model.tryThis('x');
+    assertEquals("Guesses haven't been reduced.", model.guessLeft(), 9);
+  }
+
+  @Test
+  public void testGetLetters() { 
+    model.tryThis('x');
+    assertEquals("Incorrect guess not added to string.", model.getLetters(), "x ");
+    model.tryThis('z');
+    assertEquals("Incorrect guess not added to string.", model.getLetters(), "x z ");
+  }
+
+  @Test
+  public void testTryThis() { 
+    assertEquals("Character has not been found.", model.tryThis('a'), true);
+  }
+
+  @Test
+  public void testTryWord() { 
+    assertEquals("Word has not been found.", model.tryWord("ahoy"), true);
+  }
+
+  @Test
+  public void testWon() { 
+    model.tryWord("ahoy");
+    assertEquals("Game has not been won.", model.won(), true);
   }
 
 }
